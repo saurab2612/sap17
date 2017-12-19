f55d438c71d88acf9328e4f640e8c8bca5371266
ssah33 <ssah33@GIVSARDIN38972.sapient.com>
2017-12-19 18:19:09 +0530

    Inheritance updated

---
+++ InheritanceDemo/src/com/sapient/client/EmpClient1.java
@@ -0,0 +1,26 @@
+package com.sapient.client;
+
+import com.sapient.service.Emp;
+
+public class EmpClient1 {
+
+	public static void main(String[] args) {
+		Emp e1 = new Emp(1001,"rama");
+		Emp e2 = new Emp(1002,"Amar");
+		Emp e3 = new Emp(1001,"rama");
+				
+		System.out.println(e1);
+		System.out.println(e2);
+		System.out.println(e3);
+		System.out.println(e1.equals(e2));
+		System.out.println(e1.equals(e3));
+		
+		e1=null;
+		e2=null;
+		//e3=null;
+		System.gc();
+		System.out.println((e1));
+
+	}
+
+}
---
+++ InheritanceDemo/src/com/sapient/service/Emp.java
@@ -0,0 +1,50 @@
+package com.sapient.service;
+
+public class Emp implements Cloneable {
+	
+	private int eid;
+	private String ename;
+	
+	public Emp(int eid, String ename) {
+		super();
+		this.eid = eid;
+		this.ename = ename;
+	}
+
+	@Override
+	public String toString() {
+		
+		return eid + " " + ename;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		Emp emp = (Emp)obj;
+		if(this == emp)
+			return true;
+		else if(this.eid == emp.eid && this.ename.equals(emp.ename))
+			return true;
+		else 
+			return false;
+		
+	}
+
+	@Override
+	protected void finalize() throws Throwable {
+		System.out.println("The object is garbage Collected " + eid);
+	}
+
+	@Override
+	public Emp clone() throws CloneNotSupportedException {
+		
+		return (Emp)super.clone();
+	}
+	
+	
+	
+	
+	
+	
+	
+
+}
